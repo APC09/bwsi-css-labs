d5807ebda5a8a6dbe798fe41c67a01ce95068c20
APC09 <apchitulescu@gmail.com>
2026-03-24 15:12:54 -0500

    1.1.2 changes
    I forgot to sign in so none of my commits went through

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -8,7 +8,7 @@ The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
 
 """
-
+###this is because I forgot to login so then none of my commits went through
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
     Function that takes in two numbers and an operation (add, subtract, multiply, divide),
